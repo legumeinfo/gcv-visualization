8317f66cef6418e9a9f116fd2ec69d4f8ced23d9
Alan Cleary <acleary@ncgr.org>
2020-12-11 16:21:01 -0700

    Touched up EventBus code.

--- src/common/eventbus.ts
+++ src/common/eventbus.ts
@@ -1,22 +1,28 @@
+export type EventBusSubscription = {
+  unsubscribe: () => void
+}
+
+
 // basic implementation of a publish-subscribe eventbus
 class EventBus {
 
-  private subscribers = [];
+  private _subscribers = [];
 
-  subscribe(callback) {
-    const index = this.subscribers.push(callback) - 1;
+  subscribe(callback): EventBusSubscription {
+    const index = this._subscribers.push(callback) - 1;
 
     return {
       unsubscribe: () => {
-        delete this.subscribers[index];
+        delete this._subscribers[index];
       }
     }
   }
 
-  publish(event) {
-    this.subscribers.forEach((callback) => callback(event));
+  publish(event): void {
+    this._subscribers.forEach((callback) => callback(event));
   }
 }
+
 
 // singleton
 export const eventBus = new EventBus();
